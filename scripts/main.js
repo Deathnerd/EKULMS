8ab65677c123d173a28ff6e14b2e59e77af58e5f
Wes Gilleland <wes.gilleland@gmail.com>
2013-09-25 22:05:54 -0400

    removed non-jquery ajax. Started working on render function

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,16 @@
 //Client logic goes here
-
-//prep the AJAX request
-// var req = function ajax() {
-// 	//Create new AJAX Object that supports all modern browsers
-// 	if (window.XMLHttpRequest) {
-// 		//code for everything above IE 6
-// 		request = new XMLHttpRequest;
-// 	}
-// 	else {
-// 		//code for IE 6 & 5
-// 		request = new ActiveXObject("Microsoft.XMLHTTP");
-// 	}
-
-// 	//open connection and add custom header
-// 	request.open("GET", "fetch.php", true); //asynchronous
-// 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-// 	//catch the response
-// 	request.onreadystatechange = function() {
-// 		if (request.readystate === 4 && request.status === 200) { //4 = request finished and response ready. 200 = status is "OK"
-// 			response = request.responseText; //actually catch the response
-
-// 			//if the response is null, tell me
-// 			if (response === 'null'){
-// 				console.log("response empty");
-// 			}
-// 			else {
-// 				//print the json to console *TESTING*
-// 				console.log("Here's the JSON");
-// 				console.log(response);
-// 			}
-// 		}
-
-// 		resquest.send("binary.json");
-
-// 		return response;
-// 	};
-
-	
-// }
-
-// var resp = req();
-// console.log(resp);
-
-//the jquery way
 $.ajax({
 	url: "http://www.wesgilleland.com/projects/quizzes/fetch.php",
 	success: function(result) {
 		alert("success!");
+		//put render function here
 		console.log(result);
 	},
 	data: "request=binary.json",
 	crossDomain: true
 });
+
+var render = function(json) {
+	//loop through each question
+		//loop through each choice
+}
