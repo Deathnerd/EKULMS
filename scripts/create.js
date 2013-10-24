54de3ac7c1cbef369b7f61f77be4c493b37e49ad
Wes Gilleland <wes.gilleland@gmail.com>
2013-10-24 19:22:35 -0400

    Added remove choice button. Fixed some layout bugs

--- scripts/create.js
+++ scripts/create.js
@@ -4,6 +4,12 @@
  * This will control the page to create quizzes
  */
 
+/*TODO
+ * -Fix event handlers to bind to dynamically generated elements (jquery.on() method needed)
+ * -Add radial button to the side of a question to indicate a correct answer
+ * -Oh boy... Work on function to generate json string from all the forms. Oh boy...
+ */
+
  //Globals
  var hover_id = ''; //store the current div being hovered over
  var current_choice_count = 1; //how many choices are in the current, working question?
@@ -12,7 +18,7 @@
  $('document').ready(function(){
 	//generate a new section to enter a question
 	var addChoice = function(question, prompt){
-		var prompt_data = '<br/><input type="text" name="">Enter choice</input><input type="button" value="+ choice" id="add_choice"></input>';
+		var prompt_data = '<input type="text" name="">Enter choice</input><input type="button" value="+" id="add_choice"></input><br>';
 		$("#question_"+question).append(prompt_data);
 		prompt++;
 	};
@@ -20,27 +26,50 @@
 	//Monitor for hover events over divs with the .question class
 	$('.question').hover(
 		function(){ //Hover-in handler
-			console.log($(this).attr('id'));
 			hover_id = $(this).attr('id');
+			current_choice_count = $('#'+hover_id+' > input[type="text"]').length;
+			console.log($(this).attr('id'));
 	},
 		function(){ //hover-out handler
 			console.log($(this).attr('id'));
 			hover_id = "";
-		});
+	});
 
 	//append choices to the question
 	$('.choice_add').click(function(){
 		//console.log(this);
 		current_choice_count++;
-		html = '<br/><label>Choice '+current_choice_count+'</label><input type="text" name="" value="Enter choice"></input>';
+		html = '<label id="choice_'+current_choice_count+'">Choice '+current_choice_count+'</label><input type="text" id="choice_'+current_choice_count+'" value="Enter choice"></input><br>';
 		$('#'+hover_id).append(html);
 	});
 
-	//generate a new question
+	$('.choice_remove').click(function(){
+		//console.log(this);
+		//get input and name elements in current question div and remove it
+		$('#'+hover_id+' > #choice_'+current_choice_count).remove();
+		$('#'+hover_id+' > br:last-child').remove(); //remove the break below the input
+		current_choice_count--;
+	});
+
+	//generate a new question div
 	$('.question_add').click(function(){
 		current_choice_count = 0;
 		current_question_count++;
-		html = '<div class="question" id="question_'+current_question_count+'"><p class="question_label">Question '+current_question_count+'</p><br/><textarea name="prompt"+prompt cols="40" rows="5">Input some text here</textarea><br/><label>Choice 1</label><input type="text" name="" value="Enter choice"></input><input type="button" value="+" id="add_choice" class="choice_add"></input></div>';
+		html = '<div class="question" id="question_'+current_question_count+'"><p class="question_label">Question '+current_question_count+'</p><br><textarea name="prompt"+prompt cols="40" rows="5">Input some text here</textarea><br><label>Choice 1</label><input type="text" id="choice_'+current_choice_count+'" value="Enter choice"></input><input type="button" value="+" id="add_choice" class="choice_add"></input><br></div>';
 		$('body').append(html);
 	});
+	
+	// //append choices to the question
+	// $(document).on("click", ".choice_add", function(){
+	// 	console.log(this);
+	// 	current_choice_count++;
+	// 	html = '<br/><label>Choice '+current_choice_count+'</label><input type="text" name="" value="Enter choice"></input>';
+	// 	$('#'+hover_id).append(html);
+	// });
+
+	// //when the question div is hovered over, set the global identifier for the currently hovered div
+	// $('.question').on("hover", function(){
+	// 	console.log($(this).attr('id'));
+	// 	hover_id = $(this).attr('id');
+	// });
 });
