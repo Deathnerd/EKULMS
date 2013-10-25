91382b9483d49b4a8d53ac7a2fa4c7978c06720d
Wes Gilleland <wes.gilleland@gmail.com>
2013-10-25 16:17:22 -0400

    fixed the event handlers

--- scripts/create.js
+++ scripts/create.js
@@ -5,9 +5,10 @@
  */
 
 /*TODO
- * -Fix event handlers to bind to dynamically generated elements (jquery.on() method needed)
  * -Add radial button to the side of a question to indicate a correct answer
+ * -Get the number of choices already in the div on hover and use those as values for the add/remove choice functions
  * -Oh boy... Work on function to generate json string from all the forms. Oh boy...
+ * -Figure out how to remove elements inline, not just last child. Possibly $(this).remove(); followed by a renumbering of the ids?
  */
 
  //Globals
@@ -15,52 +16,51 @@
  var current_choice_count = 1; //how many choices are in the current, working question?
  var current_question_count = 1; //how many questions have we created?
 
- $('document').ready(function(){
-	//generate a new section to enter a question
-	var addChoice = function(question, prompt){
-		var prompt_data = '<input type="text" name="">Enter choice</input><input type="button" value="+" id="add_choice"></input><br>';
-		$("#question_"+question).append(prompt_data);
-		prompt++;
-	};
-
-	//Monitor for hover events over divs with the .question class
-	$('.question').hover(
-		function(){ //Hover-in handler
-			hover_id = $(this).attr('id');
-			current_choice_count = $('#'+hover_id+' > input[type="text"]').length; //get the number of choice input elements
-			console.log($(this).attr('id'));
-	},
-		function(){ //hover-out handler
-			console.log($(this).attr('id'));
-			hover_id = "";
-	});
+ $(document).ready(function(){
 
 	//append choices to the question
-	$('.choice_add').click(function(){
-		//console.log(this);
-		current_choice_count++;
-		html = '<label id="choice_'+current_choice_count+'">Choice '+current_choice_count+'</label><input type="text" id="choice_'+current_choice_count+'" value="Enter choice"></input><br>';
-		$('#'+hover_id).append(html);
-	});
+	$(document).on({
+		click: function(){
+			current_choice_count++;
+			html = '<label id="choice_'+current_choice_count+'">Choice '+current_choice_count+'</label><input type="text" id="choice_'+current_choice_count+'" value="Enter choice"></input><br>';
+			$('#'+hover_id).append(html);
+		}
+	}, '.choice_add');
 
-	$('.choice_remove').click(function(){
-		//console.log(this);
-		//get input and name elements in current question div and remove it
-		$('#'+hover_id+' > #choice_'+current_choice_count).remove();
-		$('#'+hover_id+' > br:last-child').remove(); //remove the break below the input
-		current_choice_count--;
-	});
+	//get input and name elements in current question div and remove it
+	$(document).on({
+		click: function(){
+			$('#'+hover_id+' > #choice_'+current_choice_count).remove();
+			$('#'+hover_id+' > br:last-child').remove(); //remove the break below the input
+			current_choice_count--;
+		}
+	}, '.choice_remove');
+
+	//remove a question from the current div
+	$(document).on({
+		click: function(){
+			$('#question_'+current_question_count).remove();
+			current_question_count--;
+		}
+	}, '.question_remove');
+
+	//set the hover_id to the current question being modified
+	$(document).on({
+		mouseenter: function(){
+			hover_id = $(this).attr('id');
+		},
+		mouseleave: function(){
+			hover_id = '';
+		}
+	}, '.question');
 
 	//generate a new question div
-	$('.question_add').click(function(){
-		current_choice_count = 0;
-		current_question_count++;
-		html = '<div class="question" id="question_'+current_question_count+'"><p class="question_label">Question '+current_question_count+'</p><br><textarea name="prompt"+prompt cols="40" rows="5">Input some text here</textarea><br><label>Choice 1</label><input type="text" id="choice_'+current_choice_count+'" value="Enter choice"></input><input type="button" value="+" id="add_choice" class="choice_add"></input><br></div>';
-		$('body').append(html);
-	});
-
-	//Should be triggering for all .question class elements... hrm....
-	$(document).on("hover", ".question", function(){
-		console.log(this);
-	});
+	$(document).on({
+		click: function(){
+			current_choice_count = 0;
+			current_question_count++;
+			html = '<div class="question" id="question_'+current_question_count+'"><p class="question_label">Question '+current_question_count+'</p><br><textarea name="prompt"+prompt cols="40" rows="5">Input some text here</textarea><br><label>Choice 1</label><input type="text" id="choice_'+current_choice_count+'" value="Enter choice"></input><input type="button" value="+" id="add_choice" class="choice_add"></input><br></div>';
+			$('body').append(html);
+		}
+	}, '.question_add');
 });
