2a4bf572f249ec3dc333873187cb7564ff0a9172
Wes Gilleland <wes.gilleland@gmail.com>
2014-01-01 23:04:28 -0500

    added some error handling

--- fetch.php
+++ fetch.php
@@ -13,12 +13,17 @@
 		
 	//check to see if $_REQUEST
 	if (isset($_GET['request'])){
-		$request = $_GET['request'];
+		$request = $_GET['request']; //$request contains the name of our file
 	} else {
 		echo "request empty";
 		exit();
 	}
 
 	//read and return text from the JSON file
-	echo file_get_contents($request, true);
+	if (file_get_contents($request, true) == true){
+		echo file_get_contents($request, true);
+	}
+	else {
+		die("Cannot open file");
+	}
 ?>
