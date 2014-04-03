fa68d716bc9b14a5c02840eb0d2b9a4daf3e8b10
Wes Gilleland <wes.gilleland@gmail.com>
2014-04-03 18:05:13 -0400

    took out Db import, changed to ->close()

--- courseSignup.php
+++ courseSignup.php
@@ -8,7 +8,7 @@
 
 	session_start();
 	if (!$_SESSION['userName']) { //if a user is already signed in
-		echo("userName not set");
+		echo "userName not set";
 		exit();
 	}
 
@@ -20,25 +20,21 @@
 	if (!is_file('requires/Users.php')) {
 		die("Error in " . __FILE__ . " on line " . __LINE__ . ": Cannot find Users.php! Check your installation");
 	}
-	require_once('requires/Courses.php'); //import the user database functions
+//	require_once('requires/Users.php'); //import the user database functions
 
 	if (!is_file('requires/Courses.php')) {
 		die("Error in " . __FILE__ . " on line " . __LINE__ . ": Cannot find Courses.php! Check your installation");
 	}
 	require_once('requires/Courses.php'); //import the course database functions
 
-	//print the header
-	require('requires/header.php');
-
 	$Courses = new Courses;
-
 	if (!$Courses->courseExists($_GET['courseId'])) {
-		echo("Course does not exist");
-		die("Course does not exist");
+		echo "Course does not exist";
+		exit();
 	}
 	//insert user into course enrollment table
-	if (!$Courses->addStudent($_SESSION['courseId'], $_SESSION['userName'])) {
-		echo("Failed to add course");
+	if (!$Courses->addStudent($_GET['courseId'], $_SESSION['userName'])) {
+		echo "Failed to sign up for course. Contact administrator";
 		exit();
 	}
 	echo("Success");
