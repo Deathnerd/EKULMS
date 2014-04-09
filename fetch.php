2a2645506dea228aecd142ce4221b87ac7e7b8df
Wes Gilleland <wes.gilleland@gmail.com>
2014-04-08 21:54:09 -0400

    moved  variable to debug block

--- fetch.php
+++ fetch.php
@@ -20,13 +20,13 @@
 	}
 
 	$contents = file_get_contents($file, true);
-	$stuff = json_decode($contents, true);
 	var_dump($stuff);
 	//read and return text from the JSON file
 	if ($contents == true) {
 		//comment for debugging
 		echo $contents;
 		//uncomment for debugging
+//		$stuff = json_decode($contents, true);
 //		for ($i = 1; $i < count($stuff['quiz']['questions'][0]['choices']); $i++){
 //			echo $stuff['quiz']['questions'][0]['choices'][$i]['value']."\n";
 //		}
