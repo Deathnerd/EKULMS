0dc7aaced6bd0aba0289dbbbdc2df010d5306f7a
Wes Gilleland <wes.gilleland@gmail.com>
2014-04-08 21:50:06 -0400

    Started making import script

---
+++ import/main.py
@@ -0,0 +1,48 @@
+__author__ = 'Deathnerd'
+
+#strip the nulls from the json
+import glob, os, pprint, json, MySQLdb
+#
+# def removeNulls(filename):
+# 	quiz = json.load(open(filename))
+# 	for question in quiz['quiz']['questions']:
+# 		question['choices'].remove(None)
+# 	with open(filename, 'w') as outfile:
+# 		json.dump(quiz, outfile)
+#
+os.chdir('../quizzes')
+jsonFiles = glob.glob("*.json")
+
+# for name in jsonFiles:
+# 	removeNulls(name)
+
+#write to the db
+db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="EKULMS")
+cursor = db.cursor()
+
+for f in jsonFiles:
+	quiz = json.load(open(f))
+	#insert name of test into Tests table
+	testName = os.path.splittext(f)[0] #get just the filename and not the extension
+
+	#check if the test name already exists in the database
+	cursor.execute("SELECT * FROM Tests WHERE testName="+testName)
+	row = cursor.fetchall()
+	if row.__len__() > 0: #if it already exists in the database, skip
+		print testName+" already exists. Skipping!"
+		continue
+	#add the test name to the Tests table
+	cursor.execute("INSERT INTO Tests (courseId, testName) VALUES ('CSC185', "+testName+")")
+	#get the test id for the next step
+	cursor.execute("SELECT testId FROM Tests WHERE testName="+testName)
+	row = cursor.fetchall()
+	testId = row['testId']
+	#add the questions to the questions table
+
+cursor.execute("SELECT * FROM Tests WHERE testName='Random Stuff'")
+row = cursor.fetchall()
+if row.__len__() > 0:
+	print "This already exists"
+else:
+	cursor.execute("INSERT INTO Tests (courseId, testName) VALUES ('CSC185', 'Random Stuff')")
+
