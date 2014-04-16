428a3ffc45b3b78bdde19babf2997d7f35c0baa7
Wes Gilleland <wes.gilleland@gmail.com>
2014-04-16 15:09:44 -0400

    updated

--- requires/debug.php
+++ requires/debug.php
@@ -1,12 +1,6 @@
 <?
-//	require_once("Courses.php");
-//	$Courses = new Courses;
-//	$Courses->addInstructor("CSC191", "admin");
-
-	var_dump($_ENV);
-	var_dump($_SESSION);
-	var_dump($_SERVER);
-	var_dump($_REQUEST);
-	var_dump($_COOKIE);
-	var_dump($GLOBALS);
-	var_dump(get_headers("http://www.google.com"));
+	require_once("Tests.php");
+	require_once("Debug_Functions.php");
+	$Tests = new Tests();
+	$listOfTests = $Tests->listAll();
+	prettyPrint($listOfTests);
