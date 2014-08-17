4ec540fdfb709bdb725b8267bb74de54c6ec1c71
Wes Gilleland <wes.gilleland@gmail.com>
2014-08-17 00:23:57 -0400

    Added callback ability

--- requires/utils/utilities.php
+++ requires/utils/utilities.php
@@ -97,18 +97,24 @@
 		 * This overeager function checks if a variable is set, empty, null-string, or just plain null in that order.
 		 * If any argument meets the above requirements, the respective error is echoed out.
 		 *
-		 * @param array $vars   The variables to check. It must be an array of variables, even if the variable itself is an array
-		 * @param array $errors The respective errors to echo for each variable
+		 * @param array  $vars     The variables to check. It must be an array of variables, even if the variable itself is an array
+		 * @param array  $errors   The respective errors to echo for each variable
+		 *
+		 * @param string $callback The callback to execute if there is an error
 		 *
 		 * @return bool True if all passed, false if not
 		 */
-		public function checkIsSet($vars, $errors) {
+		public function checkIsSet($vars, $errors, $callback = null) {
 			$returnVal = true;
 			for ($i = 0; $i < count($vars); $i++) {
 				if (!isset($vars[$i]) || empty($vars[$i]) || $vars[$i] == "" || $vars[$i] == null) {
 					$returnVal = false;
 					echo $errors[$i];
 				}
+			}
+
+			if (!$returnVal && !is_null($callback)) {
+				$callback();
 			}
 
 			return $returnVal;
@@ -140,22 +146,22 @@
 		}
 
 		/**
-		 * @param Db         $Db          Requires a Database object for error checking
-		 * @param array $to               A multi-level array with the structure of
-		 *                                [
-		 *                                   [
+		 * @param Db         $Db                Requires a Database object for error checking
+		 * @param array      $to                A multi-level array with the structure of
+		 *                                      [
+		 *                                      [
 		 *                                      "name" => $name,
 		 *                                      "address" => $address,
 		 *                                      "reply_to" => $reply_to (optional)
-		 *                                   ]
-		 *                                ]
-		 * @param array      $from        An array with the structure similar to $to, but without "reply_to" key
-		 * @param string     $subject     The subject of the email
-		 * @param string     $body        The HTML body of the email
-		 * @param array      $config_vals The config values for the email. Requires the following values:
-		 *                                host, port, user_name, password
-		 * @param null|array $cc          An array with the structure similar to $from
-		 * @param null|array $bcc         An array with the sturcture similar to $cc
+		 *                                      ]
+		 *                                      ]
+		 * @param array      $from              An array with the structure similar to $to, but without "reply_to" key
+		 * @param string     $subject           The subject of the email
+		 * @param string     $body              The HTML body of the email
+		 * @param array      $config_vals       The config values for the email. Requires the following values:
+		 *                                      host, port, user_name, password
+		 * @param null|array $cc                An array with the structure similar to $from
+		 * @param null|array $bcc               An array with the sturcture similar to $cc
 		 *
 		 * @return bool
 		 */
